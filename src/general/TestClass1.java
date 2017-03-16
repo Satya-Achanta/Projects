4cc396ab774801a9d47ad13221a8d31ba6dd40f1
Satya-Achanta <satyasri.achanta@gmail.com>
2017-03-16 18:02:58 +0530

    Initail Commit
    Created new test call.

---
+++ src/general/TestClass1.java
@@ -0,0 +1,14 @@
+package general;
+
+public class TestClass1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("I am test class");
+
+	}
+
+}
